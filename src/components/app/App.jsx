310ee2e25f345145c3a7e7764bbf4eddad297781
V_Borysenko <preinspiron@gmail.com>
2023-05-14 17:57:42 +0200

    json

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,11 +5,11 @@ import SharedHeader from '../shared/shared';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import s from './App.module.scss';
 
+const Reviews = lazy(() => import('components/reviews/Reviews'));
 const Home = lazy(() => import('pages/Home'));
 const Details = lazy(() => import('pages/MovieDetails'));
 const Movies = lazy(() => import('pages/Movies'));
 const Coast = lazy(() => import('components/coast/Coast'));
-const Reviews = lazy(() => import('components/reviews/Reviews'));
 
 export const App = () => {
   return (
